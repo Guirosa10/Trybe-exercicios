223aeb17c234f4fb5f23a427f2352306143aca7c
Guilherme Rosa <gui.rsa@hotmail.com>
2021-08-27 19:15:39 -0300

    exercicios dia 4 bloco 4

---
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-3-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,9 @@
+let n = 5;
+let lista = "";
+for ( let index = 0; index < n; index += 1){
+    lista = " "
+    for(let secondIndex = 0; secondIndex < n; secondIndex += 1){
+        lista += "* ";
+         }
+         console.log(lista);
+}
