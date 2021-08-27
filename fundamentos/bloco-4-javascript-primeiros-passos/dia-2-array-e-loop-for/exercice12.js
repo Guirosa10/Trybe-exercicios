10f4f4f9d9d358622ff186543d413b91a657b7b3
Guilherme Andrade Rosa <87114377+Guirosa10@users.noreply.github.com>
2021-08-26 23:20:04 -0300

    Update

--- fundamentos/bloco-4-javascript-primeiros-passos/dia-2-array-e-loop-for/exercice12.js
+++ fundamentos/bloco-4-javascript-primeiros-passos/dia-2-array-e-loop-for/exercice12.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let newArray = [];
 for (let index = 0; index < numbers.length - 1; index += 1) {
-  newArray.push(numbers[i] + numbers[i + 1]);
+  newArray.push(numbers[index] + numbers[index + 1]);
 }
 newArray.push(numbers[numbers.lenght]*2);
 console.log(newArray);
