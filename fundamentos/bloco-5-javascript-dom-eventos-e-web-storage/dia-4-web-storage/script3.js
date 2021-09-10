249e2c79b652cbe4ed473cdc3ab755889303e860
Guilherme Rosa <gui.rsa@hotmail.com>
2021-09-10 01:34:55 -0300

    changes

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-web-storage/script3.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-web-storage/script3.js
@@ -4,6 +4,7 @@ let textSelectorH1 = document.getElementById('paragraphh1')
 
 
 function backgroundChangerFunction(){
+  document.body.style.background = 'white';
   let color = document.getElementById('colorInput').value;
   localStorage.color = color;
   document.body.style.background = localStorage.color;
