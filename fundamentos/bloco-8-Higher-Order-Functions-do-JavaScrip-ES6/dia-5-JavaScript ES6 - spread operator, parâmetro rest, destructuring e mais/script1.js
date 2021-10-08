e4d846949a5a9569cbe3382c389807873ce0ef25
Guilherme Andrade Rosa <87114377+Guirosa10@users.noreply.github.com>
2021-10-08 18:28:54 -0300

    exercices/8.5
    class exercices

---
+++ fundamentos/bloco-8-Higher-Order-Functions-do-JavaScrip-ES6/dia-5-JavaScript ES6 - spread operator, parâmetro rest, destructuring e mais/script1.js
@@ -0,0 +1,9 @@
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+const stuff = [food = bebida, lanimal = comida, drink = animal]
+//console.log(comida, animal, bebida); // arroz gato água
+console.log(`comida: ${food} , animal: ${lanimal}, bebida: ${drink}`);
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
---
+++ fundamentos/bloco-8-Higher-Order-Functions-do-JavaScrip-ES6/dia-5-JavaScript ES6 - spread operator, parâmetro rest, destructuring e mais/script2.js
@@ -0,0 +1,7 @@
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+const pares = [,,,first,second,third,fourth] = numerosPares;
+
+console.log([first,second,third,fourth]); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
